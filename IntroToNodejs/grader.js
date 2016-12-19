eb1fcf3304026f2fb43ed525bba60edcd8488cce
mp_jr1 <mapg.jr@gmail.com>
2016-12-19 21:11:55 +0000

    Added forEach verison to Average function

--- IntroToNodejs/grader.js
+++ IntroToNodejs/grader.js
@@ -1,12 +1,22 @@
 // Node Exercise 2
 // Average Grade
 
+// function average(arr){
+//         var sum = 0;
+//     for(i = 0; i < arr.length; i++){
+//         sum += arr[i];
+//     }
+//         console.log(Math.round(sum/arr.length));
+// }
+
+// ForEach version of the function
+
 function average(arr){
-        var sum = 0;
-    for(i = 0; i < arr.length; i++){
-        sum += arr[i];
-    }
-        console.log(Math.round(sum/arr.length));
+    var sum = 0;
+    arr.forEach(function(arr){
+        sum += arr;
+    });
+     console.log(Math.round(sum/arr.length));
 }
 
 var score = [90, 98, 89, 100, 100, 86, 94];
