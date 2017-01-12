4450de24b58d3ad4d7b0847f9c679cd881c2c5f4
mp_jr1 <mapg.jr@gmail.com>
2017-01-12 22:06:49 +0000

    Movie Search API Part 3 Completed

--- APIs/Movie_Search_API/app.js
+++ APIs/Movie_Search_API/app.js
@@ -1,12 +1,13 @@
 var express = require("express");
 var app = express();
 var request = require("request");
+app.set("view engine", "ejs");
 
 app.get("/results", function(req, res){
     request("http://www.omdbapi.com/?s=california", function(error, response, body){
         if(!error && response.statusCode == 200){
             var parsedData = JSON.parse(body);
-            res.send(parsedData["Search"]["0"]["Title"]);
+            res.render("results", {parsedData: parsedData});
         }
     });
 });
