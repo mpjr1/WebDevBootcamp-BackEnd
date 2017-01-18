5acbd295ca60db614e31116aa13326f8f78b95e2
mp_jr1 <mapg.jr@gmail.com>
2017-01-18 00:46:52 +0000

    Mongoose Part 2 completed

--- Databases/cats.js
+++ Databases/cats.js
@@ -11,5 +11,41 @@ var Cat = mongoose.model("Cat", catSchema);
 
 //adding a new cat ot the MongoDB
 
+// var george = new Cat({
+//     name: "Chongis",
+//     age: 10,
+//     isCute: true
+// });
+
+// george.save(function(err, cat){
+//     if(err){
+//         console.log("Something went wront");
+//     } else {
+//         console.log("We saved a cat into the DB");
+//         console.log(cat);
+//         }
+// });
+
+Cat.create({
+    name: "La Pinta",
+    age: 11,
+    isCute: true
+}, function(err, element){
+    if(err){
+        console.log("There was an error");
+    } else {
+        console.log("Element created and saved");
+        console.log(element);
+    }
+});
 
 //retrieve all cats form the MongoDB. Console.log each one
+
+Cat.find({}, function(err, element ){
+    if(err){
+        console.log("Oh No! Error!");
+    } else {
+        console.log("All the Cats");
+        console.log(element);
+    }
+})
