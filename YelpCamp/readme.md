ef1495f02f37d66da87bc653c3c1cb7eebba6a71
mp_jr1 <mapg.jr@gmail.com>
2017-03-18 03:36:09 +0000

    YelpCamp_v5_Part1 completed

--- YelpCamp/readme.md
+++ YelpCamp/readme.md
@@ -52,6 +52,10 @@
 * Add the comment new and create routes
 * Add the new comment form
 
+#Style Show Page
+* Add sidebar to show page
+* Display comments nicely
+
 
 
 RESTFUL ROUTES
--- YelpCamp/seeds.js
+++ YelpCamp/seeds.js
@@ -6,19 +6,19 @@ var data = [
     {
         name:  "Cloud's Rest",
         image: "https://farm3.staticflickr.com/2058/32163448053_79bf18abd3.jpg",
-        description: "bla bla bla"
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam bibendum augue ut sollicitudin dictum. Pellentesque arcu diam, finibus eu felis non, imperdiet pretium tortor. Curabitur ultricies fringilla purus, a scelerisque dolor laoreet interdum. In hac habitasse platea dictumst. Proin non arcu orci. Maecenas a facilisis leo. In placerat et elit at vulputate. Sed et risus augue. Aenean dui urna, aliquam ut nibh vitae, gravida tempus urna. Nullam sagittis imperdiet feugiat."
     },
     
     {
         name:  "Mountain Lake Rest",
         image: "https://farm7.staticflickr.com/6025/5982544974_a833969370.jpg",
-        description: "bla bla bla"
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam bibendum augue ut sollicitudin dictum. Pellentesque arcu diam, finibus eu felis non, imperdiet pretium tortor. Curabitur ultricies fringilla purus, a scelerisque dolor laoreet interdum. In hac habitasse platea dictumst. Proin non arcu orci. Maecenas a facilisis leo. In placerat et elit at vulputate. Sed et risus augue. Aenean dui urna, aliquam ut nibh vitae, gravida tempus urna. Nullam sagittis imperdiet feugiat."
     },
     
     {
         name:  "Canyon Floor",
         image: "https://farm5.staticflickr.com/4089/4952600040_19342714e1.jpg",
-        description: "bla bla bla"
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam bibendum augue ut sollicitudin dictum. Pellentesque arcu diam, finibus eu felis non, imperdiet pretium tortor. Curabitur ultricies fringilla purus, a scelerisque dolor laoreet interdum. In hac habitasse platea dictumst. Proin non arcu orci. Maecenas a facilisis leo. In placerat et elit at vulputate. Sed et risus augue. Aenean dui urna, aliquam ut nibh vitae, gravida tempus urna. Nullam sagittis imperdiet feugiat."
     }
 ];
 
